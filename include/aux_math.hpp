6252f1e8774c2d040ccc667cd310b8ebe1165140
dwinney <dwinney@iu.edu>
2019-01-14 16:58:59 -0500

    organized headers in include folder

---
+++ include/aux_math.hpp
@@ -0,0 +1,20 @@
+// Auxilary Math Equations
+//
+// Dependencies: None
+//
+// Author:       Daniel Winney (2018)
+// Affiliation:  Joint Physics Analysis Center (JPAC)
+// Email:        dwinney@iu.edu
+// -----------------------------------------------------------------------------
+
+#ifndef _MATH_
+#define _MATH_
+
+#include <complex>
+#include <vector>
+#include <iterator>
+#include <cmath>    //complex pow
+
+void gauleg(double x1, double x2, double x[], double w[], int n);
+
+#endif

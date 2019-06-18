ef09fa7c758d6115a08313a34dbcd837713d1c29
dwinney <dwinney@iu.edu>
2019-06-18 16:22:18 -0400

    printing iterations works

--- scripts/omega_KT.cpp
+++ scripts/omega_KT.cpp
@@ -10,14 +10,20 @@ using std::endl;
 int main()
 {
 
+// Set up the decay kinematics for the amplitude
 decay_kinematics vector_meson;
   vector_meson.set_decayJPC(1, 1, 1);
-  vector_meson.set_decayMass(0.780);
+  vector_meson.set_decayMass(.780);
   vector_meson.set_decayIsospin(0);
   vector_meson.set_decayParticle("Omega");
 
+// Omega dominated by single isobar, in general this would be a KT object that is
+// defined with a Jmax and helicity / isospin dependence.
 isobar pwave(1, 1, vector_meson);
-pwave.iterate(3);
+pwave.iterate(3); // Calculate three iterations of the KT equation
+
+// Print the results
+pwave.print(3);
 
 
 return 1.;

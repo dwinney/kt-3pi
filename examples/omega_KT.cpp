7174500f18085130bc6093ace98737f05732f681
dwinney <dwinney@iu.edu>
2019-06-26 19:54:59 -0400

    fixed wrong jpc

--- examples/omega_KT.cpp
+++ examples/omega_KT.cpp
@@ -9,12 +9,12 @@ using std::endl;
 
 int main()
 {
-  
+
   cout << endl;
 
   // Set up the decay kinematics for the amplitude
   decay_kinematics vector_meson;
-    vector_meson.set_decayJPC(1, 1, 1);
+    vector_meson.set_decayJPC(1, -1, -1);
     vector_meson.set_decayMass(.780);
     vector_meson.set_decayIsospin(0);
     vector_meson.set_decayParticle("Omega");
@@ -25,8 +25,9 @@ int main()
   pwave.iterate(3); // Calculate three iterations of the KT equation
 
   // Print the results compared to the no-3body-effects case.
+  pwave.print(0); // No rescattering
   pwave.print(1);
-  pwave.print(3);
+  pwave.print(3); // 3rd iterations
 
 
 return 1.;

dfbe2f6f8d8f84c2507a64fd694f1172a67f321f
dwinney <dwinney@iu.edu>
2019-06-27 01:08:35 -0400

    added subtraction_polynomial and iteration loops over subtractions

--- include/kt_equations.hpp
+++ include/kt_equations.hpp
@@ -42,6 +42,12 @@ private:
   // A method for evaluating the unitarity correction, i.e. the dispersion integral over the discontinuity.
   dispersion_integral disp;
 
+  // TODO: move these functions to dispersion_integral or their own class
+  constexpr complex<double> s_inelastic = xr;
+  constexpr complex<double> s_expand = 0.;
+  complex<double> conformal(double s);
+  complex<double> subtraction_polynomial(int n, double s);
+
 public:
   // TODO: KT equations depend on spin projection and helicity in general
   kt_equations(decay_kinematics dec)
--- src/kt_equations.cpp
+++ src/kt_equations.cpp
@@ -1,4 +1,4 @@
-// Class which calculates and assembles the KT solution 
+// Class which calculates and assembles the KT solution
 //
 // Dependencies: kt_iteration, kt_ang_integral, aux_math, omnes
 //
@@ -17,22 +17,56 @@ iteration kt_equations::iterate(iteration * prev)
 {
   disp.pass_iteration(prev);
 
-  vector<double> s;
-  vector<complex<double>> above, below;
-  for (int i = 0; i < interpolation::N_interp; i++)
+  vector<subtraction> subs;
+
+  for (int n = 0; n < prev->max_subs; n++)
   {
-    double s_i = sthPi + EPS + double(i) * (omnes::LamOmnes - sthPi - EPS) / double(interpolation::N_interp);
-    s.push_back(s_i);
+      vector<double> s;
+      vector<complex<double>> above, below;
+      for (int i = 0; i < interpolation::N_interp; i++)
+      {
+        double s_i = sthPi + EPS + double(i) * (omnes::LamOmnes - sthPi - EPS) / double(interpolation::N_interp);
+        s.push_back(s_i);
 
-    complex<double> ab = prev->omega(s_i, +1) * (xr + disp(s_i, +1));
-    above.push_back(ab);
+        complex<double> ab = prev->omega(s_i, +1) * (subtraction_polynomial(n, s_i) + disp(n, s_i, +1));
+        above.push_back(ab);
 
-    complex<double> be = prev->omega(s_i, -1) * (xr + disp(s_i, -1));
-    below.push_back(be);
+        complex<double> be = prev->omega(s_i, -1) * (subtraction_polynomial(n, s_i) + disp(n, s_i, -1));
+        below.push_back(be);
+      }
+
+      subtraction nth(n, s, above, below);
+      subs.push_back(nth);
   }
 
   //Save the values to a new iteration. Add one to the internal iteration number.
-  iteration next(prev->N_iteration + 1, prev->omega, s, above, below);
+  iteration next(prev->N_iteration + 1, prev->max_subs, prev->omega, subs);
 
   return next;
+};
+
+// ----------------------------------------------------------------------------
+// Conformal variable which maps the cut plane in complex s to the unit disk
+complex<double> kt_equations::conformal(double s)
+{
+  complex<double> numerator, denominator;
+
+  numerator = sqrt(s_inelastic - s_expand) - sqrt(s_inelastic - s);
+  denominator = sqrt(s_inelastic - s_expand) + sqrt(s_inelastic - s);
+
+  return numerator / denominator;
+};
+
+// ----------------------------------------------------------------------------
+// Outputs a polynomial of order n with unit coefficients in the above conformal variable
+complex<double> kt_equations::subtraction_polynomial(int n, double s)
+{
+  complex<double> result = xr;
+
+  for (int i = 1; i < n + 1; i++)
+  {
+    result += pow(conformal(s), double(i));
+  }
+
+  return result;
 };

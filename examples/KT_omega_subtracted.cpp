53454fbc87df396c9a8faca188051004e33702bc
dwinney <dwinney@iu.edu>
2019-07-05 22:54:53 -0400

    implemented kt_options and both conformal and not work! :D

--- examples/KT_omega_subtracted.cpp
+++ examples/KT_omega_subtracted.cpp
@@ -15,30 +15,37 @@ int main()
 {
   cout << endl;
 
-  // Set up the decay kinematics for the amplitude
-  decay_kinematics vector_meson;
-    vector_meson.set_decayJPC(1, -1, -1);
-    vector_meson.set_decayMass(.780);
-    vector_meson.set_decayIsospin(0);
-    vector_meson.set_decayParticle("Omega");
+// Set up the decay kinematics for the amplitude
+decay_kinematics vector_meson;
+  vector_meson.set_decayJPC(1, -1, -1);
+  vector_meson.set_decayMass(.780);
+  vector_meson.set_decayIsospin(0);
+  vector_meson.set_decayParticle("Omega");
 
-    // Similar to the unsubtracted case we start by calculating the dispersion relations
-    int n_subtractions = 1;
-    isobar kt_pwave(1, 1, n_subtractions, vector_meson);
-    kt_pwave.iterate(3); // Calculate three iterations of the KT equation
+  // Similar to the unsubtracted case we start by calculating the dispersion relations
+  // Options parameters for the KT euqations
+  kt_options options;
+  options.max_iters = 3;
+  options.max_subs = 1;
+  options.use_conformal = false;
 
-    kt_pwave.print(3, 0);
-    kt_pwave.print(3, 1);
+  isobar kt_pwave(1, 1, options, vector_meson);
+  kt_pwave.iterate(); // Calculate three iterations of the KT equation
 
-    // Start with the BESIII results
-    double BES_params[3] = {1., 120.2, 29.5};
-    poly_exp BES_fit(3, BES_params, vector_meson);
+  kt_pwave.print(0, 0);
+  kt_pwave.print(0, 1);
+  kt_pwave.print(3, 0);
+  kt_pwave.print(3, 1);
 
-    // Now we fit!!
-    dalitz_fit<poly_exp, isobar> fitter(&BES_fit, &kt_pwave);
-    fitter.extract_params(n_subtractions + 1); // Remember there is a zeroth coefficient too!!!
+  // Start with the BESIII results
+  double BES_params[3] = {1., 120.2, 29.5};
+  poly_exp BES_fit(3, BES_params, vector_meson);
 
-    kt_pwave.print_params();
+  // Now we fit!!
+  dalitz_fit<poly_exp, isobar> fitter(&BES_fit, &kt_pwave);
+  fitter.extract_params(options.max_subs + 1); // Remember there is a zeroth coefficient too!!!
+
+  kt_pwave.print_params();
 
   return 1.;
 };
